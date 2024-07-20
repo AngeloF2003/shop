026e9e719f42c0a1c6f79f45a18d372d84a6528d
Angelo907 <angelofranccesco@gmail.com>
2024-07-20 18:24:15 -0500

    3er

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
 import { NavigationItem, Category } from '../models/models';
-
+import { LoginComponent } from '../login/login.component';
+import { RegisterComponent } from '../register/register.component';
+import { Type } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  @ViewChild('modalTitle') modalTitle!: ElementRef;
+  @ViewChild('container', { read: ViewContainerRef, static: true })
+  container!: ViewContainerRef;
+
 navigationList: NavigationItem[] =[
   {
     category: "electronics",
@@ -23,4 +29,21 @@ constructor(){
 ngOnInit(): void {
 
 }
+openModal(name: string) {
+  this.container.clear();
+
+  let componentType!: Type<any>;
+  if (name === 'login') {
+    componentType = LoginComponent;
+    this.modalTitle.nativeElement.textContent = 'Iniciar sesión';
+  }
+  if (name === 'register') {
+    componentType = RegisterComponent;
+    this.modalTitle.nativeElement.textContent = 'Registrate';
+  }
+
+  this.container.createComponent(componentType);
 }
+}
+
+
